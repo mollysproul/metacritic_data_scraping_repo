0950436af9a28e95374089be181b01a7da7781aa
Molly <mfmsproul@gmail.com>
2019-08-13 17:03:59 -0400

    removed credentials.json file for privacy

--- metacritic_v3.py
+++ metacritic_v3.py
@@ -1,8 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 import datetime
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
-CURRENT_DATE = datetime.date.today()
+
+CURRENT_DATE = datetime.datetime.now()
 
 # ---------------------------------------DO-NOT-EDIT-ANYTHING-ABOVE-THIS-LINE----------------------------------------- #
 
@@ -10,48 +13,69 @@ CURRENT_DATE = datetime.date.today()
 START_DATE = CURRENT_DATE - datetime.timedelta(days=180)
 END_DATE = CURRENT_DATE + datetime.timedelta(days=180)
 
-OUTPUT_CSV = "metacritic_output.csv"
-
-
-
+OUTPUT_CSV = CURRENT_DATE.strftime('%Y%m%d') + "_metacritic_data.csv"
 
 
 # ---------------------------------------DO-NOT-EDIT-ANYTHING-BELOW-THIS-LINE----------------------------------------- #
 
-
-
-
 METACRITIC_LINK = "https://www.metacritic.com/feature/tv-premiere-dates"
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) '
                              'Chrome/50.0.2661.102 Safari/537.36'}
+
 MONTHS_DICT = {'Jan': 1, 'Feb': 2, 'March': 3, 'April': 4, 'May': 5, 'June': 6,
                'July': 7, 'August': 8, 'September': 9, 'October': 10, 'November': 11, 'December': 12}
 
 
+#labels should be consistent, (also index labels for dictionaries)
+NOTES_HEADING_LABEL = 'notes'
+NETWORK_HEADING_LABEL = 'network'
+TITLE_HEADING_LABEL = 'title'
+TYPE_HEADING_LABEL = 'type'
+LAUNCH_DATE_HEADING_LABEL = 'launch_date'
+TWITTER_TV_HEADING_LABEL = 'twitterTV_exists'
+FB_TV_HEADING_LABEL = 'fbTV_exists'
+
+# Labels of the worksheets within the "Metacritic" Spreadhseet
+ACTIVE_WORKSHEET_LABEL = "Active"
+NO_ACTION_WORKSHEET_LABEL = "NoAction"
+ARCHIVES_WORKSHEET_LABEL = "Archives"
+
+
 class airing:
     checked_this_round = 0
-    notes = 'Unknown'
+    notes = ''
     network = 'Unknown'
     title = 'Unknown'
     type = 'Unknown'
-    launch_date = 0
-    twitter_tv_exists = 0
-    fb_tv_exists = 0
+    launch_date = 'Unknown'
+    twitter_tv_exists = ''
+    fb_tv_exists = ''
     # 0 = active, 1=archives, 2=noAction
     location = -1
 
     def __iit__(self):
         self.checked_this_round = 0
-        self.notes = 'Unknown'
+        self.notes = ''
         self.network = 'Unknown'
         self.title = 'Unknown'
         self.type = 'Unknown'
-        self.launch_date = 0
-        self.twitter_tv_exists = 0
-        self.fb_tv_exists = 0
+        self.launch_date = 'Unknown'
+        self.twitter_tv_exists = ''
+        self.fb_tv_exists = ''
         # 0 = active, 1=archives, 2=noAction
         self.location = -1
 
+    def __str__(self):
+        return "checked_this_round = %d\n" \
+               "notes = %s\n" \
+               "network = %s\n" \
+               "title = %s\n" \
+               "type = %s\n" \
+               "launch_date = %s\n" \
+               "twitter_tv_exists = %s\n" \
+               "fb_tv_exists = %s\n" \
+               "location = %d\n" % (self.checked_this_round, self.notes, self.network, self.title, self.type,
+                                  self.launch_date, self.twitter_tv_exists, self.fb_tv_exists, self.location)
 
     def analyze_self(self):
         print("reanalyzing self")
@@ -81,27 +105,89 @@ class airing:
         self.fb_tv_exists = airing_data[fb_index]
 
 
-
 def main():
+    # Get airings that we have in canvs (via the Metacritic spreadhseet)
+    # Get airings listed on Metacritic Website
+    # Compare the Airings
+
+
+    #print(telemedicine)
+    #load_all_airings()
+
+    #print(archives_worksheet_data)
+
+
+
+
     print("Running main()")
+
+    #List of all of the airings currently visible on the metacritic website
     metacritic_airings = load_metacritic_airings()
 
     assert len(metacritic_airings) > 0, 'No metacritic airings found'
 
-    #for my_airing in metacritic_airings:
-        #my_airing.checked_this_round = 1
-        #if END_DATE >= my_airing.launch_date >= START_DATE:
-            # is within date range
-            # now try to find the airing in the google sheets
-            #match_found = 0
-        #print(my_airing)
-
-
     print("done running main()")
+    return
+
+# returns a dictionary of data (in "airings" class form) and indicies for the main headers
+# dict[data] = list
+# dict['notes_index']
+def load_all_airings():
+
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+    client = gspread.authorize(creds)
+    metacritic_sheet = client.open('Metacritic')
+
+    active_worksheet_data = metacritic_sheet.worksheet(ACTIVE_WORKSHEET_LABEL).get_all_records()
+    no_action_worksheet_data = metacritic_sheet.worksheet(NO_ACTION_WORKSHEET_LABEL).get_all_records()
+    archives_worksheet_data = metacritic_sheet.worksheet(ARCHIVES_WORKSHEET_LABEL).get_all_records()
+
+    spreadsheet_airings = []
+
+    for airing_dict in active_worksheet_data:
+        temp_airing = airing()
+        temp_airing.notes = airing_dict[NOTES_HEADING_LABEL]
+        temp_airing.network = airing_dict[NETWORK_HEADING_LABEL]
+        temp_airing.title = airing_dict[TITLE_HEADING_LABEL]
+        temp_airing.type = airing_dict[TYPE_HEADING_LABEL]
+        temp_airing.launch_date = airing_dict[LAUNCH_DATE_HEADING_LABEL]
+        temp_airing.twitter_tv_exists = airing_dict[TWITTER_TV_HEADING_LABEL]
+        temp_airing.fb_tv_exists = airing_dict[FB_TV_HEADING_LABEL]
+        temp_airing.location = 0
+
+        spreadsheet_airings.append(temp_airing)
+
+    for airing_dict in no_action_worksheet_data:
+        temp_airing = airing()
+        temp_airing.notes = airing_dict[NOTES_HEADING_LABEL]
+        temp_airing.network = airing_dict[NETWORK_HEADING_LABEL]
+        temp_airing.title = airing_dict[TITLE_HEADING_LABEL]
+        temp_airing.type = airing_dict[TYPE_HEADING_LABEL]
+        temp_airing.launch_date = airing_dict[LAUNCH_DATE_HEADING_LABEL]
+        temp_airing.twitter_tv_exists = airing_dict[TWITTER_TV_HEADING_LABEL]
+        temp_airing.fb_tv_exists = airing_dict[FB_TV_HEADING_LABEL]
+        temp_airing.location = 1
+        spreadsheet_airings.append(temp_airing)
+
+    for airing_dict in archives_worksheet_data:
+        temp_airing = airing()
+        temp_airing.notes = airing_dict[NOTES_HEADING_LABEL]
+        temp_airing.network = airing_dict[NETWORK_HEADING_LABEL]
+        temp_airing.title = airing_dict[TITLE_HEADING_LABEL]
+        temp_airing.type = airing_dict[TYPE_HEADING_LABEL]
+        temp_airing.launch_date = airing_dict[LAUNCH_DATE_HEADING_LABEL]
+        temp_airing.twitter_tv_exists = airing_dict[TWITTER_TV_HEADING_LABEL]
+        temp_airing.fb_tv_exists = airing_dict[FB_TV_HEADING_LABEL]
+        temp_airing.location = 2
+        print(temp_airing)
+        spreadsheet_airings.append(temp_airing)
+
+    print(len(spreadsheet_airings))
 
 
 
-
+    #return return_me
 
 def load_metacritic_airings():
     print("running load_metacritic_airings()")
@@ -130,13 +216,7 @@ def load_metacritic_airings():
     for tbody in tr_list_soup:
         #print(tbody.text)
         for tr_soup in tbody.find_all("tr", class_=["even", "sublistbig"]):
-            #print(tr_soup.text)
-            #if ", " in tr_soup.text and " / " in tr_soup.text:
-                #print(tr_soup.text)
-            # try: print(tr_soup["class"])
-            # except KeyError: print("key error")
 
-            # if th class=title in this soup
             date_soup = tr_soup.find_all("th", class_="title")
             premiere_soup = tr_soup.find_all("td", class_="title")
 
@@ -160,8 +240,8 @@ def load_metacritic_airings():
                     temp_line = temp_line[len(temp_line) - 1].split(' ')  # July 2 or July 2, 2020
                     day_int = int(temp_line[1])
 
-                    assert 1 <= month_int >= 12, "error converting this to a month: " + line
-                    assert 1 <= day_int >= 31, "error converting this to a month: " + line
+                    assert 1 <= month_int <= 12, "error converting this to a month: " + line
+                    assert 1 <= day_int <= 31, "error converting this to a month: " + line
 
                     current_date = datetime.date(year_int, month_int, day_int)
                     # print(current_date)
@@ -170,9 +250,11 @@ def load_metacritic_airings():
             elif len(premiere_soup) == 1:
                 # print("this is a premiere: ", premiere_soup[0].get_text().strip())
                 current_airing = airing()
-                current_airing.title = premiere_soup[0].get_text().strip()
+                current_airing.title = premiere_soup[0].get_text().replace("  Trailer", "").strip()
                 current_airing.launch_date = current_date
                 td_soup = tr_soup.find_all("td")
+
+
 
                 # find network
                 network_soup = td_soup[len(td_soup) - 1]
@@ -189,6 +271,7 @@ def load_metacritic_airings():
                             current_airing.type = "Special"
                         elif alt_tag == "MOVIE":
                             current_airing.type = "Movie"
+                            current_airing.fb_tv_exists = "No"
                         elif alt_tag == "NEW":
                             current_airing.type = "Series"
                         elif alt_tag == "MINISERIES":
@@ -198,15 +281,15 @@ def load_metacritic_airings():
 
                 if current_airing.type == 'Unknown':
                     current_airing.type = "Series"
-                print(current_airing.title, ", ", current_airing.type, ", ", current_airing.network,  ", ", current_airing.launch_date)
+
+                if current_airing.network == "Reelz" or current_airing.network == "Smithsonian":
+                    current_airing.notes = "Don't track this network"
+
+                print(current_airing.notes, ",", current_airing.title, ", ", current_airing.type, ", ", current_airing.network,  ", ", current_airing.launch_date, ",", current_airing.twitter_tv_exists, ",", current_airing.fb_tv_exists)
                 metacritic_airings_list.append(current_airing)
 
     #go through this list, compare to archives
     return metacritic_airings_list
-
-
-
-
 
 
 if __name__ == "__main__":
